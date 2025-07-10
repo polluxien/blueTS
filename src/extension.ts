13b0f0a30c43fd3c20992a0be152e6d6a15088b1
Sam0no <bennet.worrmann@spielarchitekten.de>
2025-07-10 13:12:21 +0200

    Find classes experimentell übernommen aus Doku und verstanden

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "Webview" is up and running now'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "redj" is now active!');
+  let webview = vscode.commands.registerCommand("helloworld.webview", () => {
+    let panel = vscode.window.createWebviewPanel("webview", "Web View", {
+      viewColumn: vscode.ViewColumn.One,
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('redj.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RedJ!');
-	});
+    // will set the html here
+    panel.webview.html = `<h1>Classes:</h1>`;
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(webview);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
