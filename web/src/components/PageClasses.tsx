b1d0aa8c41b8056b6ee82bb061461a93f23a9266
Sam0no <bennet.worrmann@spielarchitekten.de>
2025-07-14 23:10:53 +0200

    TSClassAnalyser complete and tested

---
+++ web/src/components/PageClasses.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import reactLogo from "../assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [classes, setClasses] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      const message = event.data;
+      switch (message.command) {
+        case "curClasses":
+          setClasses(message.classes);
+          setLoading(false);
+          break;
+        case "error":
+          setError(message.error);
+          setLoading(false);
+          break;
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <div>Loading...</div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>TypeScript Klassen</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        //hier Darstellung von Klassen
+        <div>
+          {classes.map((className, index) => (
+            <p key={index}>{className}</p>
+          ))}
+        </div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "",
   build: {
-    outDir: "dist",
-    emptyOutDir: true,
     rollupOptions: {
       output: {
-        // statische Dateinamen ohne Hashfür View
-        entryFileNames: "assets/index.js",
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
-  },
-});
+        entryFileNames: `assets/index.js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) {
+            return 'assets/index.css';
+          }
+          return 'assets/[name].[ext]';
+        }
+      }
+    }
+  }
+})
